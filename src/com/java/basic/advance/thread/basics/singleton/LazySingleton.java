0235bc9865b7736e4e3a670a097961cbd6066b60
RickJinny <rickjinny@126.com>
2019-11-24 22:17:36 +0800

    thread
    thread

--- src/com/java/basic/advance/thread/basics/singleton/LazySingleton.java
+++ src/com/java/basic/advance/thread/basics/singleton/LazySingleton.java
@@ -6,11 +6,26 @@ public class LazySingleton {
 
     }
 
-    private static LazySingleton lazySingleton;
+    // 加上volatile可以减少虚拟机的优化，不会再出现指令重排序问题
+    private static volatile LazySingleton lazySingleton;
 
-    public static synchronized LazySingleton getInstance() {
+    /**
+     * 双层检查加锁
+     * @return
+     */
+    public static LazySingleton getInstance() {
+        // 自旋 while(true)
         if (lazySingleton == null) {
-            lazySingleton = new LazySingleton();
+            synchronized (LazySingleton.class) {
+                if (lazySingleton == null) {
+                    lazySingleton = new LazySingleton(); // 指令重排序
+
+                    // 申请一块内存空间
+                    // 在这块空间里实例化对象
+                    // lazySingleton的引用指向这块空间地址
+
+                }
+            }
         }
         return lazySingleton;
     }
