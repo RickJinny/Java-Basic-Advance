bf760bd13bc1cc1744bf465f832329c886ebd3fe
RickJinny <rickjinny@126.com>
2019-11-24 11:46:01 +0800

    thread
    thread

--- src/com/java/basic/advance/thread/basics/bytecode/Sequence.java
+++ src/com/java/basic/advance/thread/basics/bytecode/Sequence.java
@@ -4,8 +4,31 @@ public class Sequence {
 
     private int value;
 
+    /**
+     * synchronized 放在普通方法上，内置锁就是当前类的实例
+     */
     public synchronized int getNext() {
         return value++;
+    }
+
+    /**
+     * synchronized 修饰静态方法，还能是当前类的实例吗？显然不能
+     * 内置锁是当前类的Class字节码对象 Sequence.class
+     */
+    public static synchronized int getPrevious() {
+        return 0;
+    }
+
+    public int get() {
+        // 这里只要是一个对象，就可以作为一般锁
+        // 可以使用 this、Integer.valueOf(value)、Sequence.class
+        synchronized (Sequence.class) {
+            if (value > 0) {
+                return value;
+            } else {
+                return 0;
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -33,19 +56,7 @@ public class Sequence {
                 }
             }
         }).start();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    System.out.println(Thread.currentThread().getName() + " " + sequence.getNext());
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        
         new Thread(new Runnable() {
             @Override
             public void run() {
