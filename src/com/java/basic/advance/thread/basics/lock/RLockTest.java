71cf77765f9deb4b496d23e46d9370773907746c
RickJinny <rickjinny@126.com>
2019-11-24 22:40:06 +0800

    thread
    thread

--- src/com/java/basic/advance/thread/basics/lock/RLockTest.java
+++ src/com/java/basic/advance/thread/basics/lock/RLockTest.java
@@ -1,30 +1,23 @@
 package com.java.basic.advance.thread.basics.lock;
 
+/**
+ * 重入锁例子
+ */
 public class RLockTest {
 
     public synchronized void fun1() {
         System.out.println("fun1");
-//        fun2();
+        fun2();
     }
 
     public synchronized void fun2() {
         System.out.println("fun2");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     public static void main(String[] args) {
         RLockTest rLockTest1 = new RLockTest();
-        RLockTest rLockTest2 = new RLockTest();
         new Thread(() -> {
             rLockTest1.fun1();
-        }).start();
-
-        new Thread(() -> {
-            rLockTest2.fun2();
         }).start();
     }
 }
