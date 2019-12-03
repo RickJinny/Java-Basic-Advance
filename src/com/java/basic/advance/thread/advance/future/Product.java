17f96de2613f609fcb08f60f3aecf5beaf16a5d2
RickJinny <rickjinny@126.com>
2019-12-04 00:02:11 +0800

    Future
    Future

---
+++ src/com/java/basic/advance/thread/advance/future/Product.java
@@ -0,0 +1,44 @@
+package com.java.basic.advance.thread.advance.future;
+
+public class Product {
+
+    private int id;
+
+    private String name;
+
+    public Product(int id, String name) {
+        try {
+            System.out.println("开始生产 " + name);
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.id = id;
+        this.name = name;
+        System.out.println(name + " 生产完毕...");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
