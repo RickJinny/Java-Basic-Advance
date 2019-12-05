78666990ef41936048c53fb90026bbe9cdb75a4a
RickJinny <rickjinny@126.com>
2019-12-06 00:16:57 +0800

    CopyOnWriteArrayList
    CopyOnWriteArrayList

--- src/com/java/basic/advance/thread/advance/container/ConcurrentContainerDemo.java
+++ src/com/java/basic/advance/thread/advance/container/ConcurrentContainerDemo.java
@@ -1,5 +1,7 @@
 package com.java.basic.advance.thread.advance.container;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * 并发容器
  * 1、CopyOnWriteArrayList
@@ -9,5 +11,11 @@ public class ConcurrentContainerDemo {
 
     public static void main(String[] args) {
 
+        CopyOnWriteArrayList<String> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        copyOnWriteArrayList.add("aa");
+        copyOnWriteArrayList.add("bb");
+        copyOnWriteArrayList.add("cc");
+        copyOnWriteArrayList.add("dd");
+        System.out.println(copyOnWriteArrayList);
     }
 }
