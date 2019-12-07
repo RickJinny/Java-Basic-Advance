c67f9382ee85e756e8432d761b75bf82d9d0b419
RickJinny <rickjinny@126.com>
2019-12-07 16:15:01 +0800

    ConcurrentLinkQueue
    ConcurrentLinkQueue

---
+++ src/com/java/basic/advance/thread/advance/container/ConcurrentLinkQueueDemo.java
@@ -0,0 +1,17 @@
+package com.java.basic.advance.thread.advance.container;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ *  并发容器 ConcurrentLinkQueue 的使用
+ */
+public class ConcurrentLinkQueueDemo {
+
+    public static void main(String[] args) {
+        // 创建一个 ConcurrentLinkedQueue 对象
+        ConcurrentLinkedQueue<String> concurrentLinkedQueue = new ConcurrentLinkedQueue<>();
+        
+
+
+    }
+}
