4614e73ef6efbc12ecb4221d5a7ba49c21f0bee9
RickJinny <rickjinny@126.com>
2019-12-08 23:38:19 +0800

    thread
    thread

--- src/com/java/basic/advance/thread/advance/container/ConcurrentLinkQueueDemo.java
+++ src/com/java/basic/advance/thread/advance/container/ConcurrentLinkQueueDemo.java
@@ -10,8 +10,5 @@ public class ConcurrentLinkQueueDemo {
     public static void main(String[] args) {
         // 创建一个 ConcurrentLinkedQueue 对象
         ConcurrentLinkedQueue<String> concurrentLinkedQueue = new ConcurrentLinkedQueue<>();
-        
-
-
     }
 }
