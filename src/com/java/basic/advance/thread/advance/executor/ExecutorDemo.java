b11188de71ea1db3e208d2025a81b11fcc803e16
RickJinny <rickjinny@126.com>
2019-12-14 00:15:43 +0800

    Executor
    Executor

---
+++ src/com/java/basic/advance/thread/advance/executor/ExecutorDemo.java
@@ -0,0 +1,30 @@
+package com.java.basic.advance.thread.advance.executor;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorDemo {
+
+    public static void main(String[] args) {
+
+        // 10个线程来处理大量的任务
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(10, 10, 0, TimeUnit.MICROSECONDS,
+                new LinkedBlockingDeque<>());
+
+        while (true) {
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
