8b77933157f27998150742d2465ae12ed922740d
RickJinny <rickjinny@126.com>
2019-12-14 18:12:46 +0800

    LongAddr
    LongAddr

---
+++ src/com/java/basic/advance/thread/jdk8/longaddr/LongAddrDemo.java
@@ -0,0 +1,14 @@
+package com.java.basic.advance.thread.jdk8.longaddr;
+
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * LongAddr
+ */
+public class LongAddrDemo {
+
+    public static void main(String[] args) {
+        LongAdder longAdder = new LongAdder();
+
+    }
+}
